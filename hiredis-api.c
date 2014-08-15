2fb34625e78e9280def5fc89c4b2c198f28f2e5e
Chris Williams <cwilliams@cfms4.com>
2014-08-15 15:58:26 -0700

    Add a thin layer with a few Redis commands
    Support a few basic commands:
    * SET, GET
    * INCR, DECR
    * DEL
    * WATCH, UNWATCH
    * MULTI, EXEC

---
+++ hiredis-api.c
@@ -0,0 +1,105 @@
+#include "hiredis-cluster.h"
+#include "hiredis-api.h"
+
+// watch/unwatch keys:
+redisReply *clusterWatch(clusterContext *c, const char *key)
+{
+	redisReply *reply = clusterCommand(c, "WATCH %s", key);
+	return reply;
+}
+
+redisReply *clusterUnwatch(clusterContext *c)
+{
+	redisReply *reply = clusterCommand(c, "UNWATCH");
+	return reply;
+}
+
+// transaction:
+redisReply *clusterMulti(clusterContext *c)
+{
+	redisReply *reply = clusterCommand(c, "MULTI");
+	return reply;
+}
+
+redisReply *clusterExec(clusterContext *c)
+{
+	redisReply *reply = clusterCommand(c, "EXEC");
+	return reply;
+}
+
+// get a value:
+redisReply *clusterGet(clusterContext *c, const char *key)
+{
+	redisReply *reply = clusterCommand(c, "GET %s", key);
+	return reply;
+}
+
+enum setX { SET_ALWAYS, SET_IF_EXISTS, SET_IF_NOT_EXISTS, };
+static redisReply *set(clusterContext *c, const char *key, const char *value, long long ttl, enum setX setX)
+{
+	char ttlstr[30];
+
+	int argc = 0;
+	const char *argv[10];
+
+	argv[argc++] = "SET";
+	argv[argc++] = key;
+	argv[argc++] = value;
+
+	if (ttl != 0) {
+		sprintf(ttlstr, "%lld", ttl);
+		argv[argc++] = "PX";
+		argv[argc++] = ttlstr;
+	}
+
+	if (setX == SET_IF_EXISTS) {
+		argv[argc++] = "XX";
+	} else if (setX == SET_IF_NOT_EXISTS) {
+		argv[argc++] = "NX";
+	}
+
+	redisReply *reply = NULL;
+	redisReply *clusterCommandArgv(clusterContext *cluster, int argc, const char **argv, const size_t *argvlen);
+
+	reply = clusterCommandArgv(c, argc, argv, NULL);
+
+	return reply;
+}
+
+// modify value:
+redisReply *clusterSet(clusterContext *c, const char *key, const char *value, long long ttl)
+{
+	return set(c, key, value, ttl, SET_ALWAYS);
+}
+
+redisReply *clusterSetIfExists(clusterContext *c, const char *key, const char *value, long long ttl)
+{
+	return set(c, key, value, ttl, SET_IF_EXISTS);
+}
+
+redisReply *clusterSetIfNotExists(clusterContext *c, const char *key, const char *value, long long ttl)
+{
+	return set(c, key, value, ttl, SET_IF_NOT_EXISTS);
+}
+
+redisReply *clusterIncr(clusterContext *c, const char *key)
+{
+	redisReply *reply = clusterCommand(c, "INCR %s", key);
+	return reply;
+}
+
+redisReply *clusterDecr(clusterContext *c, const char *key)
+{
+	redisReply *reply = clusterCommand(c, "DECR %s", key);
+	return reply;
+}
+
+
+// delete a key:
+redisReply *clusterDel(clusterContext *c, const char *key)
+{
+	redisReply *reply = clusterCommand(c, "DEL %s", key);
+	return reply;
+}
+
+
---
+++ hiredis-api.h
@@ -0,0 +1,27 @@
+#ifndef _HIREDIS_API_H_
+#define _HIREDIS_API_H_
+
+#include "hiredis-cluster.h"
+
+// watch/unwatch keys:
+redisReply *clusterWatch(clusterContext *c, const char *key); // reply is string "OK"
+redisReply *clusterUnwatch(clusterContext *c);
+
+// transaction:
+redisReply *clusterMulti(clusterContext *c); // reply is string "OK"
+redisReply *clusterExec(clusterContext *c); // reply is array of results (a reply for each queued command), or nil if execution was aborted
+
+// get a value:
+redisReply *clusterGet(clusterContext *c, const char *key); // reply is value of key, or nil if key does not exist
+
+// modify value:
+redisReply *clusterSet(clusterContext *c, const char *key, const char *value, long long ttl); // reply is string "OK" if value was set, or nil if value was not set
+redisReply *clusterSetIfExists(clusterContext *c, const char *key, const char *value, long long ttl); // reply is string "OK" if value was set, or nil if value was not set
+redisReply *clusterSetIfNotExists(clusterContext *c, const char *key, const char *value, long long ttl); // reply is string "OK" if value was set, or nil if value was not set
+redisReply *clusterIncr(clusterContext *c, const char *key); // reply is integer value of key after incrementing it
+redisReply *clusterDecr(clusterContext *c, const char *key); // reply is integer value of key after decrementing it
+
+// delete a key:
+redisReply *clusterDel(clusterContext *c, const char *key); // reply is integer number of keys deleted
+
+#endif
